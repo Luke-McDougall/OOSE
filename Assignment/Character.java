54a152f1c3a5bb025c9b91d1c0f606c07ece7d17
Luke McDougall <lukeyr10@gmail.com>
2020-05-07 15:44:08 +0800

    s

---
+++ Assignment/Character.java
@@ -0,0 +1,9 @@
+public class Character
+{
+    private String name;
+    private int maximumHealth = 30;
+    private List<Item> inventory = new ArrayList<>();
+    private Item equippedWeapon;
+    private Item equippedArmour;
+    private int gold = 100;
+}
